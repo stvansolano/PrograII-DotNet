1459c7525852e8cf3b7fd929054640506afc7208
Esteban Solano <stvansolano@outlook.com>
2017-08-17 14:56:31 -0600

    4. Cambios usando LINQ

--- WindowsApps/CSharp/CapaNegocio/Cliente.cs
+++ WindowsApps/CSharp/CapaNegocio/Cliente.cs
@@ -1,13 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Linq;
 
 namespace CapaNegocio
 {
     public class Cliente
     {
         public string Nombre { get; set; }
+        public string PrimerApellido { get; set; }
+        public string SegundoApellido { get; set; }
+        public string Telefono { get; set; }
+        
+        public static Cliente[] ObtenerClientes(DataTable tabla) 
+        {
+            var consulta = from cliente in tabla.AsEnumerable()
+                           select new Cliente {
+                               Nombre = cliente.Field<string>("Nombre"),
+                               Telefono = cliente.Field<string>("Telefono")
+                           };
+
+            return consulta.ToArray();
+        }
     }
 }
